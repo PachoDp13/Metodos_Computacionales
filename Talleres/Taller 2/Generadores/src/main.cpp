dd8d64f7f4011ad33a1a0bd3eae5a57c226b48a0
PachoDp13 <fj.diaz@uniandes.edu.co>
2021-03-26 22:42:12 -0500

    Generadores c++

--- Talleres/Taller 2/Generadores/src/main.cpp
+++ Talleres/Taller 2/Generadores/src/main.cpp
@@ -6,7 +6,6 @@ int main(){
 
         Random *r1 = new Random(); 
         Random *r2 = new Random(); 
-        string str = "Método";
 	
         r1->SetSeed(6625);
         r2->SetSeed(6625);
@@ -14,9 +13,9 @@ int main(){
         r1->SetMethod("simple");
         r2->SetMethod("drand48");
 
-	for(int i = 0; i < 2; i++)
+	for(int i = 0; i < 1000; i++)
 		//std::cout << r->rand(-10.,10.) << std::endl;
-		std::cout << r1->rand()  << std::endl;
+		std::cout << r1->rand()  << " " << r2->rand() << std::endl;
 
 
 	delete r1;
